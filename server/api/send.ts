48ab8903cd1ca59c700fba4e4ba458774ae17bf7
Jayan Ratna <jayanratna@gmail.com>
2023-06-21 16:38:05 +1200

    Update `from` and `to` parameter usage

--- server/api/send.ts
+++ server/api/send.ts
@@ -5,8 +5,8 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export default defineEventHandler(async () => {
   try {
     const data = await resend.emails.send({
-      from: 'onboarding@resend.dev',
-      to: 'delivered@resend.dev',
+      from: 'Acme <onboarding@resend.dev>',
+      to: ['delivered@resend.dev'],
       subject: "Hello world",
       html: "<strong>It works!</strong>",
     });
