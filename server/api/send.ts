89af732c4f71360f62d9ab1f10621466d93c6b73
Bu Kinoshita <bukinoshita@gmail.com>
2025-06-04 15:22:00 -0300

    feat: add biome

--- server/api/send.ts
+++ server/api/send.ts
@@ -7,8 +7,8 @@ export default defineEventHandler(async () => {
     const data = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
       to: ['delivered@resend.dev'],
-      subject: "Hello world",
-      html: "<strong>It works!</strong>",
+      subject: 'Hello world',
+      html: '<strong>It works!</strong>',
     });
 
     return data;
