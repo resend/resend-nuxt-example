5a7b868c1ec91ebb0550037c2e6134ad0e5a1d83
gabriel miranda <gabrielmfern@outlook.com>
2025-10-31 12:52:19 -0300

    just return

--- server/api/send.ts
+++ server/api/send.ts
@@ -2,8 +2,8 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default defineEventHandler(async () => {
-  return await resend.emails.send({
+export default defineEventHandler(() => {
+  return resend.emails.send({
     from: 'Acme <onboarding@resend.dev>',
     to: ['delivered@resend.dev'],
     subject: 'Hello world',
