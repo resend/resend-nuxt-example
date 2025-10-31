a74e96e59710e6e2a3b8495cbcd1e4c6f60a5c31
gabriel miranda <gabrielmfern@outlook.com>
2025-10-31 12:48:13 -0300

    chore: proper error handling
    we've not been throwing errors for some time now

--- server/api/send.ts
+++ server/api/send.ts
@@ -3,16 +3,10 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export default defineEventHandler(async () => {
-  try {
-    const data = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
-      subject: 'Hello world',
-      html: '<strong>It works!</strong>',
-    });
-
-    return data;
-  } catch (error) {
-    return { error };
-  }
+  return await resend.emails.send({
+    from: 'Acme <onboarding@resend.dev>',
+    to: ['delivered@resend.dev'],
+    subject: 'Hello world',
+    html: '<strong>It works!</strong>',
+  });
 });
