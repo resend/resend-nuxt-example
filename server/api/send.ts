ef1e6ceb1d20ba526949df4fe8cb76eae9019a17
Gabriel Miranda <gabrielmfern@outlook.com>
2025-11-11 17:54:25 -0300

    chore: renovate (#71)

--- server/api/send.ts
+++ server/api/send.ts
@@ -1,18 +1,12 @@
-import { Resend } from 'resend';
+import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default defineEventHandler(async () => {
-  try {
-    const data = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
-      subject: 'Hello world',
-      html: '<strong>It works!</strong>',
-    });
-
-    return data;
-  } catch (error) {
-    return { error };
-  }
+export default defineEventHandler(() => {
+	return resend.emails.send({
+		from: "Acme <onboarding@resend.dev>",
+		to: ["delivered@resend.dev"],
+		subject: "Hello world",
+		html: "<strong>It works!</strong>",
+	});
 });
