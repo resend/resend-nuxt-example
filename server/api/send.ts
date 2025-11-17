91e78533045eca9aac8493fa81de7f2066d7e0ae
Alireza Jahandideh <ar.jahandideh@gmail.com>
2025-11-17 06:01:45 -0800

    fix: return using Nuxt/h3 createError function (#15)
    Co-authored-by: Gabriel Miranda <gabrielmfern@outlook.com>

--- server/api/send.ts
+++ server/api/send.ts
@@ -2,11 +2,20 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export default defineEventHandler(() => {
-	return resend.emails.send({
+export default defineEventHandler(async () => {
+	const response = await resend.emails.send({
 		from: "Acme <onboarding@resend.dev>",
 		to: ["delivered@resend.dev"],
 		subject: "Hello world",
 		html: "<strong>It works!</strong>",
 	});
+
+  if (response.error) {
+    throw createError({
+      statusCode: 500,
+      message: 'Error sending email'
+    });
+  }
+
+  return response;
 });
